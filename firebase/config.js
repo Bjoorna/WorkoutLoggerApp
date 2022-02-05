c34c682a059bba588a2871a6d373e64c1746af63
Bjoorna <marcusbjorna@gmail.com>
2022-02-05 15:35:05 +0100

    removed config

--- firebase/config.js
+++
@@ -1,7 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAf6yOGx_V5wXyVUzot1sDgsICKPbDVgIs",
-    authDomain: "workoutlogger-48f71.firebaseapp.com",
-    databaseURL: "https://workoutlogger-48f71-default-rtdb.europe-west1.firebasedatabase.app/",
-    storageBucket: "gs://workoutlogger-48f71.appspot.com",
-    projectId: "workoutlogger-48f71"
-};
