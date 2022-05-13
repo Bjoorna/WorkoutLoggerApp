808d7e3357ea4554e05346153dd73e6c568a22c1
Bjoorna <marcusbjorna@gmail.com>
2022-05-13 12:39:19 +0200

    Update TextButton.js

--- components/Buttons/TextButton.js
+++ components/Buttons/TextButton.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Pressable, StyleSheet, Vibration } from "react-native";
+import { useSelector } from "react-redux";
+
 
 import { Themes } from "../../shared/Theme";
 import LabelText from "../Text/Label";
-const theme = Themes.dark;
 
 const TextButton = (props) => {
+	const useDarkMode = useSelector((state) => state.appSettings.useDarkMode);
+	const [styles, setStyles] = useState(
+		getStyles(useDarkMode ? Themes.dark : Themes.light)
+	);
+	const [currentTheme, setCurrentTheme] = useState(
+		useDarkMode ? Themes.dark : Themes.light
+	);
+
 	const [isPressed, setIsPressed] = useState(false);
+
+	useEffect(() => {
+		setStyles(getStyles(useDarkMode ? Themes.dark : Themes.light));
+		setCurrentTheme(useDarkMode ? Themes.dark : Themes.light);
+	}, [useDarkMode]);
 	const isDisabled = props.disabled;
 
 	const handleOnPressIn = () => {
@@ -23,7 +37,10 @@ const TextButton = (props) => {
 					...props.style,
 				}}
 			>
-				<LabelText large={true} style={{...styles.disabledText, ...props.textStyle}}>
+				<LabelText
+					large={true}
+					style={{ ...styles.disabledText, ...props.textStyle }}
+				>
 					{props.children}
 				</LabelText>
 			</Pressable>
@@ -40,7 +57,10 @@ const TextButton = (props) => {
 				onPressOut={() => setIsPressed(false)}
 				onPress={props.onButtonPress}
 			>
-				<LabelText style={{...styles.text, ...props.textStyle}} large={true}>
+				<LabelText
+					style={{ ...styles.text, ...props.textStyle }}
+					large={true}
+				>
 					{props.children}
 				</LabelText>
 			</Pressable>
@@ -48,43 +68,45 @@ const TextButton = (props) => {
 	}
 };
 
-const styles = StyleSheet.create({
-	baseButtonStyle: {
-		height: 40,
-		minWidth: 48,
-		paddingHorizontal: 12,
-		borderRadius: 20,
-		overflow: "hidden",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	pressedButtonStyle: {
-		height: 40,
-		minWidth: 48,
-		paddingHorizontal: 12,
-		borderRadius: 20,
-		overflow: "hidden",
-		alignItems: "center",
-		justifyContent: "center",
-		backgroundColor: theme.primary,
-		opacity: 0.12,
-	},
-	disabledButtonStyle: {
-		height: 40,
-		minWidth: 48,
-		paddingHorizontal: 12,
-		borderRadius: 20,
-		overflow: "hidden",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	disabledText: {
-		color: theme.onSurface,
-		opacity: 0.38,
-	},
-	text: {
-		color: theme.primary,
-	},
-});
+const getStyles = (theme) => {
+	return StyleSheet.create({
+		baseButtonStyle: {
+			height: 40,
+			minWidth: 48,
+			paddingHorizontal: 12,
+			borderRadius: 20,
+			overflow: "hidden",
+			alignItems: "center",
+			justifyContent: "center",
+		},
+		pressedButtonStyle: {
+			height: 40,
+			minWidth: 48,
+			paddingHorizontal: 12,
+			borderRadius: 20,
+			overflow: "hidden",
+			alignItems: "center",
+			justifyContent: "center",
+			backgroundColor: theme.primary,
+			opacity: 0.12,
+		},
+		disabledButtonStyle: {
+			height: 40,
+			minWidth: 48,
+			paddingHorizontal: 12,
+			borderRadius: 20,
+			overflow: "hidden",
+			alignItems: "center",
+			justifyContent: "center",
+		},
+		disabledText: {
+			color: theme.onSurface,
+			opacity: 0.38,
+		},
+		text: {
+			color: theme.primary,
+		},
+	});
+};
 
 export default TextButton;
